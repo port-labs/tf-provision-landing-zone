18d7948b68827b64d7b00e8a5522f86bae07b522
Mor Paz <mor@getport.io>
2023-01-20 18:27:41 +0000

    add new landing zone for account morp-test-lz

---
+++ morp-test-lz.tf
@@ -0,0 +1,41 @@
+# Copyright Amazon.com, Inc. or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+module "sandbox_account_01" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail = "mor@getport.io"
+    AccountName  = "morp-test-lz"
+    # Syntax for top-level OU
+    ManagedOrganizationalUnit = "Sandbox"
+    # Syntax for nested OU
+    # ManagedOrganizationalUnit = "Sandbox (ou-xfe5-a8hb8ml8)"
+    SSOUserEmail     = "mor@getport.io"
+    SSOUserFirstName = "mor"
+    SSOUserLastName  = "paz"
+  }
+
+  account_tags = {
+    "ABC:Owner"       = "mor@getport.io"
+    "ABC:Division"    = "ENT"
+    "ABC:Environment" = "Dev"
+    "ABC:CostCenter"  = "123456"
+    "ABC:Vended"      = "true"
+    "ABC:DivCode"     = "102"
+    "ABC:BUCode"      = "ABC003"
+    "ABC:Project"     = "123456"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "mor paz"
+    change_reason       = "testing the account vending process"
+  }
+
+  custom_fields = {
+    custom1 = "a"
+    custom2 = "b"
+  }
+
+  account_customizations_name = "sandbox-customizations"
+}
